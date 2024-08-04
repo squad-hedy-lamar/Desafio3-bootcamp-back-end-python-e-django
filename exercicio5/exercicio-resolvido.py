0c6478c1682b0400219f49b1f509516042a3f87b
natachabkr1 <natachabkr@hotmail.com>
2024-08-04 20:36:05 -0300

    desafio3_exercicio5

---
+++ exercicio5/exercicio-resolvido.py
@@ -0,0 +1,8 @@
+# 5. Crie duas tuplas. Concatene-as para formar uma nova tupla.
+
+lista_tarefas1 = ('realizar trabalho 1', 'realizar estudo 3')
+lista_tarefas2 = ('ir no supermercado', 'prepapar o jantar')
+
+concatenacao_tuplas = lista_tarefas1 +  lista_tarefas2
+
+print(concatenacao_tuplas)
